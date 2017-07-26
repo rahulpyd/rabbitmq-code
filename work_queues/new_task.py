1c253ed3918d83a661ed174b353b106b27dd8694
Rahul P <rahupyd@gmail.com>
2017-07-26 17:30:25 +0530

    work_queues

---
+++ work_queues/new_task.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import sys
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='task_queue')
+
+message = ' '.join(sys.argv[1:]) or "Hello World!"
+channel.basic_publish(exchange='',
+                      routing_key='task_queue',
+                      body=message,
+                      properties=pika.BasicProperties(
+                         delivery_mode = 2, # make message persistent
+                      ))
+print(" [x] Sent %r" % message)
+
+connection.close()
